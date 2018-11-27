ec9e62295a8e5325f28e00cdb04cbba7c1610e07
Muhammed Eldeeb <meldeeb1998@gmail.com>
2018-11-28 01:19:40 +0200

    create a get_3_minmum methods

--- Project/src/ReturnValues.java
+++ Project/src/ReturnValues.java
@@ -3,5 +3,13 @@
  * @author Eldeeb
  */
 public class ReturnValues {
+    public final int first;
+    public final int seconde;
+    public final int third;
     
+    public ReturnValues(int f , int s , int t){
+        first = f;
+        seconde = s;
+        third = t;
+    }
 }
