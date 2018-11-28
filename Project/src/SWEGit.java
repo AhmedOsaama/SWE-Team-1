90a3b5ab419d1f2777952ddadf7a86870b2aaa68
Ahmed <ahmedosama49@gmail.com>
2018-11-28 19:29:35 +0200

    My Function: Reversed Array

--- Project/src/SWEGit.java
+++ Project/src/SWEGit.java
@@ -2,23 +2,42 @@ import java.util.*;
 public class SWEGit {
 	public  int[] ReverseArray(int[] arr)
 	{
-		int[] array = new int[arr.length];
+		int[] array = new int[arr.length]; //initializing an array of the same size of input array(arr).
 		for (int i=arr.length-1; i>=0; i--)
 		{
-			array[(arr.length-1)-i] = arr[i];
+			array[(arr.length-1)-i] = arr[i];		//assigns elements of the input array to the new array in a reversed manner.
 		}
 		
 		return array;
 	}
 	
 	public char[] ReverseArray(char[] arr) {
-		char[] array = new char[arr.length];
+		char[] array = new char[arr.length];			//initializing an array of the same size of input array(arr).
 		for (int i=arr.length-1; i>=0; i--)
 		{
-			array[(arr.length-1)-i] = arr[i];
+			array[(arr.length-1)-i] = arr[i];			//assigns elements of the input array to the new array in a reversed manner.
 		}
 		return array;
 		
+	}
+	
+	
+	//Methods to print the Reversed Array.
+	
+	public void printArray(int[] arr)
+	{
+		for(int i=0; i<arr.length;i++)
+		{
+			System.out.print(arr[i]);
+		}
+	}
+	
+	public void printArray(char[] arr)
+	{
+		for(int i=0; i<arr.length;i++)
+		{
+			System.out.print(arr[i]);
+		}
 	}
 	
 }
